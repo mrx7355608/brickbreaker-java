ae3f42a774f0208698fd9df7e779e09c85c59003
mrx7355608 <mrx7355608@gmail.com>
2024-06-24 16:48:03 +0500

    moving base left and right

--- src/gui/Base.java
+++ src/gui/Base.java
@@ -9,6 +9,8 @@ import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -22,17 +24,18 @@ import javax.swing.JButton;
  *
  * @author fawad
  */
-public class Base extends JButton {
+public class Base extends JButton implements KeyListener {
     private final int BASE_WIDTH = 130;
     private final int BASE_HEIGHT = 20;
-    private int x = 400;
-    private int y = 400;
-    private int velocity = 1;
+    public int x = 400;
+    public int y = 400;
+    private int velocity = 0;
     
     
     public Base() {
         super.setPreferredSize(new Dimension(BASE_WIDTH, BASE_HEIGHT));
         super.setBounds(x, y, BASE_WIDTH, BASE_HEIGHT); // set initial x & y coordinates of base
+        super.addKeyListener(this);
 
         
         // Load base image
@@ -47,5 +50,38 @@ public class Base extends JButton {
     }
 
     public void move() {
+        this.x += this.velocity;
     }
+
+    public int getBASE_WIDTH() {
+        return BASE_WIDTH;
+    }
+
+    public int getBASE_HEIGHT() {
+        return BASE_HEIGHT;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_RIGHT) {
+            System.out.println("right");
+            this.velocity = 5;
+        }
+        if (code == KeyEvent.VK_LEFT) {
+            System.out.println("left");
+            this.velocity = -5;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    
+    
 }
--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -9,32 +9,40 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener {
 
     private final int PANEL_WIDTH = 800;
     private final int PANEL_HEIGHT = 500;
-    
-    BackgroundImageLabel backgroundImageLabel;
+    private final BackgroundImageLabel backgroundImageLabel;
+    private final Base base;
+    private final Timer timer;
 
     public GamePanel() {
         super.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         super.setLayout(new BorderLayout());
 
         backgroundImageLabel = new BackgroundImageLabel();
-        backgroundImageLabel.add(new Base());
-        
+        base = new Base();
+        timer = new Timer(16, this);
+        backgroundImageLabel.add(base);
+
         super.add(backgroundImageLabel, BorderLayout.NORTH);
+        timer.start();
     }
 
-//    @Override
-//    public void paint(Graphics g) {
-//        super.paint(g);
-//        g.drawImage(backgroundImage, 0, 0, null);
-//    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-//        this.repaint();
+        this.repaint();
+        
+        base.move();
+        base.setBounds(base.x, base.y, base.getBASE_WIDTH(), base.getBASE_HEIGHT());
     }
 
 }
