461447bc889aad49f4f0eb410b4be6f02fd5f584
mrx7355608 <mrx7355608@gmail.com>
2024-06-30 00:18:33 +0500

    fixed collision glitches

--- src/gui/Ball.java
+++ src/gui/Ball.java
@@ -1,4 +1,3 @@
-
 package gui;
 
 import java.awt.Color;
@@ -11,20 +10,23 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Ball extends JButton {
+
     private final int BALL_WIDTH = 20;
     private final int BALL_HEIGHT = 15;
     public int xVelocity = 4;
     public int yVelocity = -4;
     public int x = 325;
     public int y = 350;
-    
+    private final long COLLISION_COOLDOWN = 100;
+    private long lastCollisionTime = 0;
+
     public Ball() {
         super.setPreferredSize(new Dimension(BALL_WIDTH, BALL_HEIGHT));
         super.setBounds(x, y, BALL_WIDTH, BALL_HEIGHT);
         super.setBackground(new Color(255, 255, 255, 0));
         super.setBorderPainted(false);
         super.setFocusable(false);
-        
+
         // Load ball image
         try {
             Image baseImage = ImageIO.read(new File("assets/ball.png")).getScaledInstance(BALL_WIDTH, BALL_HEIGHT, Image.SCALE_SMOOTH);
@@ -33,19 +35,63 @@ public class Ball extends JButton {
             System.out.println("[ERROR] Unable to load ball image");
         }
     }
-    
+
     public void moveBall() {
-        if (this.x < 0 || (this.x +this.getBALL_WIDTH()) > 790) {
+        if (this.x < 0 || (this.x + this.getBALL_WIDTH()) > 790) {
             this.xVelocity *= -1;
         }
         if (this.y < 0) {
             this.yVelocity *= -1;
         }
-        
+
         this.x += this.xVelocity;
         this.y += this.yVelocity;
         this.setBounds(this.x, this.y, this.BALL_WIDTH, this.BALL_HEIGHT);
 
+    }
+
+    public void checkCollisionsWithBase(Base base) {
+        // Using time base collision checks to avoid some collision glitches
+        // like, multiple collision detections at a time
+        long currentTime = System.currentTimeMillis();
+
+        // CHECK COLLISION WITH TOP SIDE OF THE BASE
+        if ((currentTime - this.lastCollisionTime) > this.COLLISION_COOLDOWN) {
+            if (this.x >= base.x
+                    && this.x <= base.x + base.getBASE_WIDTH()
+                    && this.y + this.getBALL_HEIGHT() >= base.y
+                    && this.y + this.getBALL_HEIGHT() <= base.y + base.getBASE_HEIGHT()) {
+                System.out.println("normal");
+                this.yVelocity *= -1;
+
+                if (this.x > 400) {
+                    this.xVelocity *= -1;
+                }
+                this.lastCollisionTime = currentTime;
+
+            } // CHECK COLLISIONS WITH LEFT SIDE OF THE BASE
+            else if (this.x + this.getBALL_WIDTH() >= base.x
+                    && this.x + this.getBALL_WIDTH() <= base.x + base.getBASE_WIDTH() / 4
+                    && this.y >= base.y
+                    && this.y <= base.y + base.getBASE_HEIGHT()) {
+                System.out.println("left side");
+                this.xVelocity *= -1;
+                this.yVelocity *= -1;
+                this.lastCollisionTime = currentTime;
+
+            } // CHECK COLLISIONS WITH RIGHT SIDE OF THE BASE (!!! Not tested yet !!!)
+            else if (this.x <= base.x + base.getBASE_WIDTH()
+                    && this.x >= base.x + base.getBASE_WIDTH() - base.getBASE_WIDTH() / 4
+                    && this.y + this.getBALL_HEIGHT() >= base.y
+                    && this.y + this.getBALL_HEIGHT() <= base.y + base.getBASE_HEIGHT()) {
+                System.out.println("Right side");
+                this.xVelocity *= -1;
+                this.yVelocity *= -1;
+                this.lastCollisionTime = currentTime;
+
+            }
+
+        }
     }
 
     public int getBALL_WIDTH() {
@@ -55,5 +101,5 @@ public class Ball extends JButton {
     public int getBALL_HEIGHT() {
         return BALL_HEIGHT;
     }
-    
+
 }
--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -22,8 +22,6 @@ public class GamePanel extends JPanel implements ActionListener {
     private final Ball ball;
     ArrayList<Brick> bricks = new ArrayList();
     private final Timer timer;
-    private final long COLLISION_COOLDOWN = 100;
-    private long lastCollisionTime = 0;
 
     public GamePanel() {
         super.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
@@ -52,41 +50,10 @@ public class GamePanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         super.repaint();
 
-        long currentTime = System.currentTimeMillis();
-
         base.move();
         ball.moveBall();
-
-        // Check ball's collision with base
-        // Change ball direction if base's top collides with ball's bottom
-        if (ball.x >= base.x
-                && ball.x <= base.x + base.getBASE_WIDTH()
-                && ball.y + ball.getBALL_HEIGHT() >= base.y
-                && ball.y + ball.getBALL_HEIGHT() <= base.y + 1) {
-            System.out.println("normal");
-            ball.yVelocity *= -1;
-
-            if (ball.x > 400) {
-                ball.xVelocity *= -1;
-            }
-        }
-        
-        if ((currentTime - lastCollisionTime) > COLLISION_COOLDOWN) {
-            if (ball.x + ball.getBALL_WIDTH() >= base.x
-                    && ball.x + ball.getBALL_WIDTH() <= base.x + 4
-                    && ball.y + ball.getBALL_HEIGHT() >= base.y
-                    && ball.y + ball.getBALL_HEIGHT() <= base.y + 5) {
-                System.out.println("left side");
-                ball.xVelocity *= -1;
-                ball.yVelocity *= -1;
-                lastCollisionTime = currentTime;
-            }
-        }
-        
-        if (ball.y >= 500) {
-            timer.stop();
-            JOptionPane.showMessageDialog(this, "Gameover");
-        }
+        ball.checkCollisionsWithBase(base);
+        this.checkGameover();
 
 //        if (ball.x <= base.x + base.getBASE_WIDTH() && ball.y >= base.y && ball.y <= base.y + base.getBASE_HEIGHT()) {
 //            ball.xVelocity *= -1;
@@ -107,6 +74,13 @@ public class GamePanel extends JPanel implements ActionListener {
                 ball.yVelocity *= -1;
                 break;
             }
+        }
+    }
+
+    private void checkGameover() {
+        if (ball.y >= 500) {
+            timer.stop();
+            JOptionPane.showMessageDialog(this, "Gameover");
         }
     }
 
